6a7b7da2ca027f9927d24030af22ed84addeb9cb
Gozaking <122152700+Gozaking@users.noreply.github.com>
2024-05-14 19:47:13 +0900

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Vector3 potato;
+    public Vector3 vec;
     // Start is called before the first frame update
     void Start()
     {
