f8be15b3ee35c6b564bf63b7c27bd0698ceae91e
Gozaking <122152700+Gozaking@users.noreply.github.com>
2024-05-14 19:44:21 +0900

    Update NewBehaviourScript.cs

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-<<<<<<< HEAD
     public Vector3 Position;
-=======
-    public Transform transform;
->>>>>>> b01658e3a6a3e6eeba7b7f14d2a8e12324ceca1d
     // Start is called before the first frame update
     void Start()
     {
