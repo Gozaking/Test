f003156b11b7cbaf708ebb6e8375fc7bd8c2e479
Gozaking <122152700+Gozaking@users.noreply.github.com>
2024-05-29 00:00:59 +0900

    ytg

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Transform veg;
+    public Transform veq;
     // Start is called before the first frame update
     void Start()
     {
